ec08d5f9e52a325abac99b413a6dd51194d94f12
Rilwan <Rilwanhaq@gmail.com>
2022-03-02 15:32:31 +0530

    new commita

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,48 @@
 import React from 'react'
 import './/home.css';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+
 
 var arr = [];
 function Home() {
   const { register, handleSubmit } = useForm();
 
+  const [ name, setName] = useState([]);
+  const [ age, setAge] = useState([]);
+  const [ occupation, setOccupation] = useState([]);
+  const [ profile, setProfile] = useState([]);
+  const [ address, setAddress] = useState([]);
+  const [ mobile, setMobile] = useState([]);
+  const [ gender, setGender] = useState([]);
+
+  function handle ()  {
+    localStorage.setItem('Name', name);
+    localStorage.setItem('Age', age);
+    localStorage.setItem('Occupation',occupation);
+    localStorage.setItem('Profile',profile);
+    localStorage.setItem('Address',address);
+    localStorage.setItem('Mobile',mobile);
+    localStorage.setItem('Gender',gender);
+  };
+  const remove = () => {
+    localStorage.removeItem('Name');
+    localStorage.removeItem('Age');
+    localStorage.removeItem('Occupation');
+    localStorage.removeItem('Profile');
+    localStorage.removeItem('Address');
+    localStorage.removeItem('Mobile');
+    localStorage.removeItem('Gender');
+  }
+
   const onSubmit = (data) => {
     arr.push(data);
     console.log(arr);
+  }
+  let navigate = useNavigate();
+  function viewChange(){
+    navigate('/view')
   }
 
   return (
@@ -16,27 +50,24 @@ function Home() {
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <div className="form-group">
           <label className='pad'>Enter Name :</label>
-          <input  {...register('Name',{ required: true })} className="form-control" id="name" placeholder="Enter Name" />
+          <input  value={name}{...register('Name',{ required: "enter name please" })} className="form-control" id="name" placeholder="Enter Name" onChange={(e) => setName(e.target.value)}/>
           <label className='pad'>Enter Age :</label>
-          <input type="number" {...register('Age',{ required: true })} className="form-control" id="age" placeholder="Enter Age" />
+          <input value={age} type="number" {...register('Age',{ required: true })} className="form-control" id="age" placeholder="Enter Age" onChange={(e) => setAge(e.target.value)}/>
           <label className='pad'>Enter Occupation :</label>
-          <input type="text" {...register('Occupation',{ required: true })} className="form-control" id="occupation" placeholder="Enter Occupation" />
+          <input value={occupation} type="text" {...register('Occupation',{ required: true })} className="form-control" id="occupation" placeholder="Enter Occupation" onChange={(e) => setOccupation(e.target.value)} />
           <label className='pad'>Add Profile Image</label>
-          <input type="file" {...register('Profile Image',{ required: true })} className="form-control" id="profileImage" placeholder="Add Profile Image" />
+          <input value={profile} type='file' accept='image/*'  multiple="false" {...register('Profile Image',{ required: true })} className="form-control" id="profileImage" placeholder="Add Profile Image" onChange={(e) => setProfile(e.target.value)}/>
           <label className='pad'>Enter Address :</label>
-          <input type="text" {...register('Address',{ required: true })} className="form-control" id="address" placeholder="Enter Address" />
+          <input value={address} type="text" {...register('Address',{ required: true })} className="form-control" id="address" placeholder="Enter Address" onChange={(e) => setAddress(e.target.value)}/>
           <label className='pad'>Enter Mobile Number :</label>
-          <input type="text" {...register('Mobile Number',{ required: true })} className="form-control" id="phone" placeholder="Enter Mobile Number" />
-          <label className='pad'>Enter Phone Number :</label>
-          <input type="text" {...register('Phone Number',{ required: true })} className="form-control" id="phone" placeholder="Enter Phone Number"/>
+          <input value={mobile} type="text" {...register('Mobile Number',{ required: true })} className="form-control" id="phone" placeholder="Enter Mobile Number" onChange={(e) => setMobile(e.target.value)}/>
           <label className='pad'>Select Gender :</label>
-          <select >
-            <option value="Male" {...register('GENDER',{ required: true })} >Male</option>
-            <option value="Fmale" {...register('GENDER',{ required: true })}>Female</option>
-            <option value="Other" {...register('GENDER',{ required: true })}>Other</option>
-          </select>
+          <input value={gender} type='text' {...register('Gender',{required: true})} className="form-control" id='gender' placeholder='Enter Gender' onChange={(e) => setGender(e.target.value)} />
           <label className='pad'>
-          <input type="submit"/>
+          <input type="submit"  onClick={()=>{
+            handle()
+            viewChange()
+          }} />
           </label>
           </div>
           </form>
--- src/components/view.js
+++ src/components/view.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { useEffect } from 'react';
 
 
 function View() {
-
-
+  useEffect (() => {
+    const name = localStorage.getItem('Name');
+    const age = localStorage.getItem('Age');
+    const occupation = localStorage.getItem('Occupation');
+    const profile = localStorage.getItem('Profile');
+    const address = localStorage.getItem('Address');
+    const mobile = localStorage.getItem('Mobile');
+  },[])
 
   return (
     <>
-    <h1>HEllo From View</h1>
+      <div className="container">
+        <div className="row">
+          <h1></h1>
+        </div>
+      </div>
+
     </>
   )
 }
